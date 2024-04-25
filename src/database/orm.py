b27f2d79c430fbe6cad8454c346ba3088c50d8b5
leerobing <ghqls654@gmail.com>
2024-04-25 16:53:30 +0900

    feat: post 로직 구현

--- src/database/orm.py
+++ src/database/orm.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Boolean,Column,Integer,String
 from sqlalchemy.orm import declarative_base
 
+from schema.request import CreateTodoRequest
+
 Base = declarative_base()
 
 class Todo(Base):
@@ -11,3 +13,12 @@ class Todo(Base):
 
     def __repr__(self):
         return f"Todo(id={self.id},contents={self.contents},is_done={self.is_done}))"
+
+    # Pydantic 객체를 ORM 객체로 변환하기 위한 클레스 메서드
+    @classmethod
+    def create(cls, request : CreateTodoRequest) -> "Todo":
+        return cls(
+            contents=request.contents,
+            is_done=request.is_done
+        )
+
--- src/database/repository.py
+++ src/database/repository.py
@@ -3,10 +3,15 @@ from typing import List
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from database.orm import Todo
-from sqlalchemy import insert
+
 
 def get_todos(session : Session) -> List[Todo]:
     return list(session.scalars(select(Todo)))
 
 def get_todo_by_todo_id(session : Session, todo_id : int) -> Todo | None:
     return session.scalar(select(Todo).where(Todo.id == todo_id))
+def create_todo(session : Session, todo : Todo) -> Todo:
+    session.add(instance=todo) # 세션 객체에 db에 저장하고자 하는 객체를 저장한다.
+    session.commit() # 이시점에서 db에 반영된다.
+    session.refresh(instance=todo) #pk id 값이 할당된 todo 객체를 read 한다.
+    return todo
